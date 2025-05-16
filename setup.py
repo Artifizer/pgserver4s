23b91278e79109f62a45b124ad2a66bc480f7553
Artifizer <artifizer@google.com>
2025-05-16 15:02:03 +0300

    Update setup.py to set proper package name and version

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
 from setuptools import setup
 
+# This setup.py is configured to read metadata from pyproject.toml
 setup(
+    name="pgserver4s",
+    version="0.1.0",
+    description="Self-contained postgres server for your python applications",
     setup_requires=["cffi"],
     # dummy but needed for the binaries to work
     cffi_modules=["src/pgserver4s/_build.py:ffibuilder"],
